596ba954c255fda6a4c7c920d8e428a6b6136b45
Christopher Hunt <christopher.hunt333@gmail.com>
2026-07-03 17:21:12 -0500

    Videos 4, 5, and 6 Completed

---
+++ Assets/Scripts/ResourceCounter.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using TMPro;
+
+public class ResourceCounter : MonoBehaviour
+{
+    public TextMeshProUGUI appleText;
+    public TextMeshProUGUI oreText;
+
+    private int apples;
+    private int ores;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        UpdateUI();
+    }
+
+    private void UpdateUI()
+    {
+        appleText.text = "Apples: " + apples;
+        oreText.text = "Ores: " + ores;
+    }
+
+    public void AddResource(string resourceName, int amount)
+    {
+        if (resourceName == "Apple")
+        {
+            apples += amount;
+        }
+        else if (resourceName == "Ore")
+        {
+            ores += amount;
+        }
+
+        UpdateUI();
+    }
+}
